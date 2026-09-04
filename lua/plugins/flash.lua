294b9a88fb50f93876bbf5fefd7afb84242a7298
Artur Markiewicz <artur.m@posteo.de>
2026-09-05 00:35:47 +0200

    feat: Flash plugin added
    fix: Flash

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,26 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  vscode = true,
+  ---@type Flash.Config
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- Simulate nvim-treesitter incremental selection
+    {
+      "<C-s>",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter({
+          actions = {
+            ["<C-s>"] = "next",
+            ["<BS>"] = "prev"
+          }
+        })
+      end,
+      desc = "Treesitter Incremental Selection"
+    },
+  },
+}
