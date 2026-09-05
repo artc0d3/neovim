e706b2e0bd4cbfb4d8f3c0ba982ba90429a9c435
Artur Markiewicz <artur.m@posteo.de>
2026-09-05 13:38:38 +0200

    feat: Blink cmp added

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,26 @@
+return {
+  "saghen/blink.cmp",
+  dependencies = { "folke/lazydev.nvim" },
+  version = "*",
+  ---@module "blink.cmp"
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = {
+      preset = "default",
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+    },
+    sources = {
+      default = { "lazydev", "lsp", "path", "buffer" },
+      providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          score_offset = 100,
+        },
+      },
+    },
+    completion = {
+      documentation = { auto_show = true },
+    },
+  },
+}
